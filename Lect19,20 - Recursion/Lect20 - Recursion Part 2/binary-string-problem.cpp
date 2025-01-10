f2492c3c6f2800dba14ebe50de9cdd47a21ef5ba
CaptainShorya <shoryavishnoi13@gmail.com>
2025-01-10 20:09:41 +0530

    Assignment Added

--- Lect19,20 - Recursion/Lect20 - Recursion Part 2/binary-string-problem.cpp
+++ Lect19,20 - Recursion/Lect20 - Recursion Part 2/binary-string-problem.cpp
@@ -17,8 +17,23 @@ void binString(int n, int lastplace, string ans){
     }
 }
 
+void binString2(int n, string ans){
+    if( n == 0 ){
+        cout << ans << endl;
+        return;
+    }
+
+    if(ans[ans.size()-1] != '1'){ //If we use == '0', we do not get all desired output as it will initially run the else loop always which is not correct.
+        binString2(n-1, ans + '0');
+        binString2(n-1, ans + '1');
+    }else{
+        binString2(n-1, ans + '0');
+    }
+}
+
 int main(){
     string ans = "" ;
     binString(3,0,ans);
+    binString2(3,ans);
     return 0;
 }
