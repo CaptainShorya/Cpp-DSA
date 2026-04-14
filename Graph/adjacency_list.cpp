4aaeb7a361801af5b27a5fef08825624427034ee
CaptainShorya <shoryavishnoi13@gmail.com>
2026-04-14 16:52:33 +0530

    Adjacency set inside adjacency_map.cpp

--- Graph/adjacency_list.cpp
+++ Graph/adjacency_list.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 
 vector<list<pair<int,int>>> graph;
-int v;
+int v; // No. of vertices
 
 void add_edge(int src, int dest, int wt, bool bi_dir=true){
-    graph[src].push_back({dest,wt});
+    // graph[i] → list of neighbours of node i
+    // Adds an edge from src to dest with weight wt, and if bidirectional, also adds reverse edge
+    graph[src].push_back({dest,wt}); 
     if(bi_dir){
         graph[dest].push_back({src,wt});
     }
@@ -16,8 +18,8 @@ void add_edge(int src, int dest, int wt, bool bi_dir=true){
 void display(){
     for(int i=0; i<graph.size(); i++){
         cout << i << " -> ";
-        for(auto el : graph[i]){
-            cout <<"("<<  el.first << "," << el.second << ")"<< ",";
+        for(auto el : graph[i]){ // Iterate ith index linked list 
+            cout <<"("<<  el.first << "," << el.second << ")"<< ","; //el.first → destination node, el.second → weight of edge
         }
         cout << "\n";
     }
--- Graph/adjacency_map.cpp
+++ Graph/adjacency_map.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 vector<unordered_map<int,int>> graph;
@@ -13,11 +14,23 @@ void add_edge(int src, int dest, int wt, bool bi_dir=true){
     }
 }
 
+// Display for directed/undirected(depends on bi_dir) adjacency map graph
 void display(){
     for(int i=0; i<graph.size(); i++){
         cout << i << " -> ";
         for(auto el : graph[i]){
             cout <<"("<<  el.first << "," << el.second << ")"<< ",";
+        }
+        cout << "\n";
+    }
+}
+
+// Display for undirected adjacency set graph
+void display_set(vector<unordered_set<int>> &undirected){
+    for(int i=0; i<undirected.size(); i++){
+        cout << i << " -> ";
+        for(auto el : undirected[i]){
+            cout << el << ",";
         }
         cout << "\n";
     }
@@ -33,5 +46,22 @@ int main(){
         cin >> s >> d >> wt;
         add_edge(s,d,wt,false); // Directed
     }
+    cout << "Directed Weighted Graph:\n";
     display();
+
+    // How can I convert a adjacency map directional to undirected adjacency set graph...
+    vector<unordered_set<int>> undirected(v);
+
+    for(int i = 0; i < v; i++){
+        for(auto el : graph[i]){
+            int u = i;
+            int vtx = el.first;
+
+            undirected[u].insert(vtx);
+            undirected[vtx].insert(u); // reverse edge
+        }
+    }
+
+    cout << "\nUndirected Unweighted Graph:\n";
+    display_set(undirected);
 }
