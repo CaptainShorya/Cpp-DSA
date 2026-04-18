d629391ca44a59c3e11da9c624d9b90919148a29
CaptainShorya <shoryavishnoi13@gmail.com>
2026-04-18 23:38:40 +0530

    Floor Fill 2nd Approach

--- Graph/floos_fill.cpp
+++ Graph/floos_fill.cpp
@@ -18,3 +18,24 @@ public:
         return image;
     }
 };
+
+//Approach 2 
+class Solution {
+public:
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int originalColor){
+        if(sr < 0 || sc < 0 || sr == image.size() || sc == image[0].size()) return;
+        if(image[sr][sc] != originalColor) return; // If adjacent element not same as of originalColor ,return back(No change required)
+
+        if(image[sr][sc] == color) return; //Avoids Infinite Recursion possiblity
+        image[sr][sc] = color;
+        dfs(image,sr-1,sc,color,originalColor);
+        dfs(image,sr,sc-1,color,originalColor);
+        dfs(image,sr+1,sc,color,originalColor);
+        dfs(image,sr,sc+1,color,originalColor);
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        dfs(image,sr,sc,color,image[sr][sc]);
+        return image;
+    }
+};
